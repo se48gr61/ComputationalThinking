cb359335c1fe5bed3753ef2dd8b10cb11cadd466
se48gr61 <se48gr61@isdedu.de>
2015-09-09 09:08:22 +0200

    Wrote this small userInput program.

--- LetterToNumber.py
+++ LetterToNumber.py
@@ -3,3 +3,21 @@ letter=input("Type a letter: ")
 
 pos = alphabet.find(letter)
 print(pos +1)
+
+myString = "Finished"
+print(myString)
+
+#Numbers
+
+numbers = "123456789101112"
+userInput = input("Type a Number: ")
+
+pos = numbers.find(userInput)
+print(pos)
+
+myString = "Done"
+print(myString)
+
+
+
+
